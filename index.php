eba3b0ce26feabe6bd5de82f00da6b5b1be0f9fc
Enis Gjini <egjini17@gmail.com>
2025-01-09 23:32:45 +0100

    Update store closed modal to prevent dismissal and improve user experience

--- index.php
+++ index.php
@@ -945,12 +945,12 @@ try {
 
 <body>
     <?php if ($is_closed && isset($_SESSION['selected_store'])): ?>
-        <div class="modal fade" id="storeClosedModal" tabindex="-1" aria-labelledby="storeClosedModalLabel" aria-hidden="true">
+        <div class="modal fade" id="storeClosedModal" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false"  aria-labelledby="storeClosedModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title"><?= htmlspecialchars($notification['title']) ?></h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                        <!-- <button type="button" class="btn-close" data-bs-dismiss="modal"></button> -->
                     </div>
                     <div class="modal-body">
                         <?= htmlspecialchars($notification['message']) ?>
